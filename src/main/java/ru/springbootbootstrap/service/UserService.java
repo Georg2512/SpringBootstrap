e6c618e78df2e76ba575025f1535aab852c1f7fd
Georg Gribov FullStack <Sektor162s@mail.ru>
2022-06-18 09:52:33 +0400

    BootstrapV1

---
+++ src/main/java/ru/springbootbootstrap/service/UserService.java
@@ -0,0 +1,19 @@
+package ru.springbootbootstrap.service;
+
+import ru.springbootbootstrap.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    void saveUser(User user);
+
+    void updateUser(User user);
+
+    void deleteUser(long id);
+
+    List<User> getAllUsers();
+
+    User getUserByLogin(String login);
+
+    User getUserById(long id);
+}
